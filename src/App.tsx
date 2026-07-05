754175a6160d72b9008f06e9adc3e40bc51ff928
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-07-05 18:47:03 +0000

    feat: set up react routing for dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-
+// App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { DashboardScreen } from "./features/dashboard/DashboardScreen";
 import './App.css'
 
 function App() {
- 
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/dashboard" element={<DashboardScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
