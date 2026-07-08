15312dee1616e9ac6604bbc5197d9b39f3bd08f6
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-07-08 13:00:40 +0000

    fix: wrap auth screens in AuthLayout and correctly nest routes under  MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  { AuthLayout } from "./layouts/AuthLayout";
 import { MainLayout } from "./layouts/MainLayout";
 import { DashboardScreen } from "./features/dashboard/DashboardScreen";
 import {LoginScreen} from "./features/auth/LoginScreen";
@@ -10,28 +11,35 @@ import { SubjectScreen } from "./features/subjects/SubjectScreen";
 import { TopicScreen } from "./features/subjects/TopicScreen";
 import { QuizScoreScreen } from "./features/quiz/QuizScoreScreen";
 import { QuizScreen } from "./features/quiz/QuizScreen";
-import "./App.css";
 import { EditProfileScreen } from "./features/profile/EditProfileScreen";
 import { ProfileScreen } from "./features/profile/ProfileScreen";
+import "./App.css";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Auth flow */}
+        <Route element={<AuthLayout/>}>
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/signup" element={<SignUpScreen />} />
+          <Route path="/select-grade" element={<SelectGradeScreen />} />
+        </Route>
+
+         {/* Main app */}
         <Route element={<MainLayout />}>
           <Route path="/dashboard" element={<DashboardScreen />} />
+          <Route path="/subject/:subjectId" element={<SubjectScreen />} />
+          <Route path="/subject/:subjectId/topic/:topicId" element={<TopicScreen/>}/>
+          <Route path="/subject/:subjectId/topic/:topicId/quiz" element={<QuizScreen />} />
+          <Route path="/subject/:subjectId/topic/:topicId/quiz-score" element={<QuizScoreScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          <Route path="/edit-profile" element={<EditProfileScreen />} />
         </Route>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/signup" element={<SignUpScreen />} />
-        <Route path="/select-grade" element={<SelectGradeScreen />} />
-        <Route path="/subject/:subjectId" element={<SubjectScreen />} />
-        <Route path="/subject/:subjectId/topic/:topicId" element={<TopicScreen/>}/>
-        <Route path="/subject/:subjectId/topic/:topicId/quiz" element={<QuizScreen />} />
-        <Route path="/subject/:subjectId/topic/:topicId/quiz-score" element={<QuizScoreScreen />} />
-        <Route path="/profile" element={<ProfileScreen />} />
-        <Route path="/edit-profile" element={<EditProfileScreen />} />
+      
+        
       </Routes>
     </BrowserRouter>
   );
